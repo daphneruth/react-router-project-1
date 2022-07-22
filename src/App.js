f92ebabea2aac9f6b38e0099b45e24647ff5d334
Daphne <daphneruth304@gmail.com>
2022-07-22 18:08:01 +0300

    completed defining all routes required

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
    import {Route, Switch} from 'react-router-dom';
+   import AllQuote from './pages/AllQuotes';
+   import NewQuote from './pages/NewQuote';
+   import QuoteDetails from './pages/QuoteDetails';
    function App() {
       return (
         <Switch>
           <Route path='/quotes'>
-
+          <AllQuote/>
           </Route>
 
           <Route path = '/quotes/:quoteID'>
+            <QuoteDetails/>
 
           </Route>
 
           <Route path=' /new-quote'>
+            <NewQuote/>
 
           </Route>
 
