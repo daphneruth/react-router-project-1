7a5cd2b8e1715c197baf1bbc4cca0f41cb33285a
Daphne <daphneruth304@gmail.com>
2022-07-22 18:22:57 +0300

    obtaining Id using useParams hook

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,4 +1,12 @@
-const QuoteDetails = ()=>{
-    <h1>Quote details</h1>
-}
-export default QuoteDetails;
+    import { Fragment } from "react";
+    import { Fragment } from "react";
+    import { useParams } from "react-router-dom";
+    const QuoteDetails = ()=>{
+        const params = useParams();
+
+       return <Fragment>
+       < h1>Quote details</h1>
+       <p>{params.quoteId}</p>
+       </Fragment>
+    }
+    export default QuoteDetails;
