6f82a63054aef8b9119d6f1a20493596b326d503
Daphne <daphneruth304@gmail.com>
2022-07-22 18:15:42 +0300

    redirecting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-   import {Route, Switch} from 'react-router-dom';
+   import {Route, Switch, Redirect} from 'react-router-dom';
    import AllQuote from './pages/AllQuotes';
    import NewQuote from './pages/NewQuote';
    import QuoteDetails from './pages/QuoteDetails';
    function App() {
       return (
         <Switch>
+          <Route path ='/' >
+            <Redirect to ='/quotes'/>
+          </Route>
           <Route path='/quotes' exact>
           <AllQuote/>
           </Route>
