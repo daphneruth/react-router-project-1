53ea6f64d10d2ed6f400cad3bed19e95ec77108f
Daphne <daphneruth304@gmail.com>
2022-07-23 12:32:42 +0300

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
    import {Route,Switch ,Redirect} from 'react-router-dom';
    import AllQuote from './pages/AllQuotes';
-   import NewQuote from './pages/NewQuote';
+   
    import QuoteDetails from './pages/QuoteDetails';
    import Layout from './components/layout/Layout';
 
