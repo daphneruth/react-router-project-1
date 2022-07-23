6fcc6c2ec42390330f1c7fb10fc8d69b8e315f26
Daphne <daphneruth304@gmail.com>
2022-07-23 07:25:23 +0300

    updated

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,3 +1,16 @@
+import QuoteList from '../components/quotes/QuoteList';
+const DUMMY_QUOTES = [
+    {
+        id:'q1',
+        author:'ME',
+        text:'react is fun'
+    },
+    {
+        id:'q1',
+        author:'you',
+        text:'react is great'
+    },
+]
 const AllQuote = ()=>{
     <h1>All quotes page</h1>
 }
