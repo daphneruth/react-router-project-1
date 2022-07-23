54a984afcb54d1c2a4e937df74ed624556e6949b
Daphne <daphneruth304@gmail.com>
2022-07-23 07:11:38 +0300

    updated

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import classes from './Layout.module.css';
+import MainNavigation from "./MainNavigation";
+const Layout =()=>{
+    return
+    <Fragment>
+        <main>
+
+        </main>
+    </Fragment>
+
+}
+export default Layout
