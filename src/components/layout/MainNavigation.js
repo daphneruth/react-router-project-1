5c7d2e88a5ee2c24b8880d23966b661d4b43aede
Daphne <daphneruth304@gmail.com>
2022-07-23 07:07:50 +0300

    navigation using NavLink

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,11 +1,20 @@
+import { NavLink } from "react-router-dom";
+import classes from './MainNavigation.module.css';
 const MainNavigation = ()=>{
 return 
-<header>
-    <div>
-        <nav>
+<header  className={classes.header}>
+    <div className={classes.logo}>
+        <nav className={classes.nav}>
             <ul>
                 <li>
-                    
+                 <NavLink to ='/quotes' activeClassName={classes.active}>
+                    AllQuotes
+                 </NavLink>
+                </li>
+                <li>
+                 <NavLink to ='/new-quotes' activeClassName={classes.active}>
+                    Add a quote
+                 </NavLink>
                 </li>
             </ul>
         </nav>
