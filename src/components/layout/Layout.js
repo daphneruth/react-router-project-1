2ed139946eb52cdf635b79222962bd3a238ed72e
Daphne <daphneruth304@gmail.com>
2022-07-23 08:08:04 +0300

    fixed some bugs

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,6 +4,7 @@ import MainNavigation from "./MainNavigation";
 const Layout =(props)=>{
     return
     <Fragment>
+        <MainNavigation/>
         <main className={classes.main}>
          {props.children}
         </main>
--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -2,14 +2,30 @@
     import { Fragment } from "react";
     import { useParams } from "react-router-dom";
     import Comments from '../components/comments/Comments';
-    impo
+    import { Route } from "react-router-dom";
+    import HighlightedQuote from '../components/quotes/HighlightedQuote'
+
+    const DUMMY_QUOTES = [
+        {
+            id:'q1',
+            author:'ME',
+            text:'react is fun'
+        },
+        {
+            id:'q1',
+            author:'you',
+            text:'react is great'
+        },
+    ]
     const QuoteDetails = ()=>{
         const params = useParams();
+
+        
 
        return <Fragment>
        < h1>Quote details</h1>
        <p>{params.quoteId}</p>
-       <Route path ={`/quotes/${params.quoteId}/comments`}>
+       <Route path ='quotes/:quoteId/Comments'>
         
        </Route>
        </Fragment>
